718f1f568308ee43ee3589226bd941c1f01bead3
unknown <marielena.velascoenriquez@student.kuleuven.be>
2022-03-28 14:43:56 +0200

    contour modifications

--- Example_mv01.py
+++ Example_mv01.py
@@ -1,37 +1,50 @@
 import cv2 as cv
+from cv2 import THRESH_BINARY
 import numpy as np
+#import utlis
 
 img = cv.imread('Photos/mosfet.jpg')
 cv.imshow('Mosfet',img)
 
 blank = np.zeros(img.shape[:2], dtype = 'uint8')
-cv.imshow('Blank',blank)
+#cv.imshow('Blank',blank)
 
 #Converting to grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+#cv.imshow('Gray', gray)
 
 #Blur
 #blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
 #cv.imshow('Blur',blur)
 
-#Edge cascades
-canny = cv.Canny(img, 125, 125)
+#Edge cascades / only converts edge not interiors
+#canny = cv.Canny(img, 125, 125)
 #canny = cv.Canny(blur, 125, 125)
-cv.imshow('Canny edges', canny)
+#cv.imshow('Canny edges', canny)
 
 # Dilating the image
-#dilated = cv.dilate(canny, (3,3), iterations=3)
+#dilated = cv.dilate(canny, (3,3), iterations=1)
 #cv.imshow('Dilated', dilated)
 
-ret, thresh = cv.threshold(gray,125,125, cv.THRESH_BINARY)
-cv.imshow('Thresh',thresh)
+#Erode
+#erode = cv.erode(canny, np.ones((5,5)), iterations=5)
+#cv.imshow('Erode', erode)
 
-contours, hierarchies = cv.findContours(canny, cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
-print(f'{len(contours)} contour(s) found!')
+ret, thresh1 = cv.threshold(gray,130,255, cv.THRESH_BINARY)
+#thresh2 = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY,11,7)
+cv.imshow('Thresh1',thresh1)
+#cv.imshow('Thresh2',thresh2)
 
-cv.drawContours(blank, contours, -1, (0,0,255), 2)
-cv.imshow('Countors Drawn', blank)
+contours, hierarchies = cv.findContours(thresh1, cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+
+#utlis.getContours(img,showCanny=True)
+
+
+
+#contours, hierarchies = cv.findContours(thresh1, cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+#print(f'{len(contours)} contour(s) found!')
+
+#cv.drawContours(blank, contours, -1, (255,255,255), 1)
+#cv.imshow('Countors Drawn', blank)
 
 cv.waitKey(0)
-
